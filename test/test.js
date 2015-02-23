f3651503d1e7e204d56cab0635b1b2b7f16f2544
Rodrigo Alviani <rodrigoalviani@gmail.com>
2015-02-23 20:40:05 -0300

    Add more tests

--- test/test.js
+++ test/test.js
@@ -5,12 +5,22 @@ var should = require('should')
   , corner = require('../lib/corner');
 
 describe('corner', function () {
-  it('should return a corner', function () {
+  it('should return a corner (SouthEast)', function () {
   request('http://guiadossolteiros.com/wp-content/uploads/2013/08/mulher-lavando-carro-520x245.jpg')
     .on('response', function (img) {
       corner(img, function (err, c) {
         if (err) throw err;
         c.should.equal('SouthEast');
+      });
+    });
+  });
+
+  it('should return a corner (NorthEast)', function () {
+  request('http://www.theartnewspaper.com/imgart/Lowther-Castle.jpg')
+    .on('response', function (img) {
+      corner(img, function (err, c) {
+        if (err) throw err;
+        c.should.equal('NorthEast');
       });
     });
   });
